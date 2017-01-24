816082065d220b23ab515c586d5a1df262da0929
DanielLChang <daniellawrencechang@gmail.com>
2017-01-24 00:02:58 -0800

    Add animejs

---
+++ frontend/animations.js
@@ -0,0 +1,8 @@
+import anime from 'animejs';
+import Howler from './howler';
+
+const Animations = (ctx, root) => {
+
+};
+
+export default Animations;
--- frontend/dropit.js
+++ frontend/dropit.js
@@ -1,7 +1,11 @@
+import Animations from './animations';
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const ctx = root.getContext('2d');
   ctx.root.width = window.innerWidth;
   ctx.root.height = window.innerHeight;
+
+  Animations(ctx, root);
 
 });
